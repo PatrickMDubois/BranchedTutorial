e18c180de81107f0c0e331a438d9bce8226af5f7
Patrick Dubois <patrick.marcel.dubois@gmail.com>
2015-06-09 15:00:47 -0500

    Images appear when clicking for more.

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -8,8 +8,8 @@ module.run(["$templateCache", function($templateCache) {
     "    <div class=\"content\">\n" +
     "        <div>{{element.text[0]}}</div>\n" +
     "        <div ng-mouseover=\"overImage=true\" ng-mouseleave=\"overImage=false\" class=\"image-container\">\n" +
-    "            <img src=\"{{element.screenshots[0]}}\" />\n" +
-    "            <div class=\"image-tag\" ng-show=\"overImage\">See {{element.screenshots.length}} images.</div>\n" +
+    "            <img src=\"{{element.images[0]}}\" />\n" +
+    "            <div class=\"image-tag\" ng-show=\"overImage\" ng-click=\"seeImages=true\">See {{element.images.length}} images.</div>\n" +
     "        </div>\n" +
     "    </div>\n" +
     "    <div class=\"notes\">\n" +
@@ -17,6 +17,9 @@ module.run(["$templateCache", function($templateCache) {
     "            {{note}}\n" +
     "            <hr ng-if=\"!$last\" />\n" +
     "        </div>\n" +
+    "    </div>\n" +
+    "    <div ng-show=\"seeImages\">\n" +
+    "        <img ng-repeat=\"image in element.images\" src=\"{{image}}\" />\n" +
     "    </div>\n" +
     "</div>");
 }]);
